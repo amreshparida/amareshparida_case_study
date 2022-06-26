5db7fa532119cb31115795d9fbf289aaef6a017f
Amaresh Parida <59873364+amreshparida@users.noreply.github.com>
2022-06-26 13:44:53 +0530

    Try Catch and request validation implemented
    Try Catch and request validation

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -7,36 +7,71 @@ use Illuminate\Http\Request;
 
 //using User model
 use App\Models\User;
-
+use Validator;
 
 class AuthController extends Controller
 {
     //Login method to validate auth/login input credential
     public function login(Request $request){
-       $data = [ 'email' => $request->email, 'password' => $request->password ];
 
-       if(auth()->attempt($data))
-       {
-            //generating token for user
-            $token = auth()->user()->createToken('Token')->accessToken;
+    //Implement try catch
+        try{
 
-            //Return JSON logged in success response with access token
-            return response()->json([
-                'success' => true,
-                'message' => 'Logged in successfully',
-                'data' =>   [
-                                'token' => $token
-                            ]
-                ],);
-       }
-       else{
-        // return log in failled JSON response
-        return response()->json([
-            'success' => false,
-            'message' => 'Invalid email or password',
-            'error_code' => 401
-            ], 401);
-       }
+            //using validator for request fields validations
+            $validator = Validator::make($request->all(),[
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if($validator->fails()){
+
+                $error = $validator->errors()->all()[0];
+                return response()->json([
+                    'success' => false,
+                    'message' => $error,
+                    'error_code' => 422
+                ],422);
+
+            }else{
+
+                $data = [ 'email' => $request->email, 'password' => $request->password ];
+
+                if(auth()->attempt($data))
+                {
+                     //generating token for user
+                     $token = auth()->user()->createToken('Token')->accessToken;
+         
+                     //Return JSON logged in success response with access token
+                     return response()->json([
+                         'success' => true,
+                         'message' => 'Logged in successfully',
+                         'data' =>   [
+                                         'token' => $token
+                                     ]
+                         ],);
+                }
+                else{
+                 // return log in failled JSON response
+                 return response()->json([
+                     'success' => false,
+                     'message' => 'Invalid email or password',
+                     'error_code' => 401
+                     ], 401);
+                }
+
+            }
+
+
+        } catch (\Exception $e){
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => $e->getMessage(),
+                    'error_code' => 400
+                ], 500
+            );
+        }
+      
 
     }
 }
